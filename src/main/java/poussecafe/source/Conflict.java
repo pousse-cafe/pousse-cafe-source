d05fd2cafd987186c26e6158783aa0f4ff10a9ec
Gérard Dethier <info@gerarddethier.be>
2024-07-08 16:21:21 +0200

    chore: drop lombok.

--- src/main/java/poussecafe/source/Conflict.java
+++ src/main/java/poussecafe/source/Conflict.java
@@ -1,16 +1,45 @@
 package poussecafe.source;
 
-import lombok.Builder;
-import lombok.Value;
-import lombok.experimental.Accessors;
+import java.util.Objects;
+
 import poussecafe.source.analysis.ClassName;
 
-@Builder
-@Value
-@Accessors(fluent = true)
 public class Conflict {
 
+    public ClassName outerModuleClass() {
+        return outerModuleClass;
+    }
+
     private ClassName outerModuleClass;
-    
+
+    public ClassName innerModuleClass() {
+        return innerModuleClass;
+    }
+
     private ClassName innerModuleClass;
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+
+        private Conflict conflict = new Conflict();
+
+        public Conflict build() {
+            Objects.requireNonNull(conflict.outerModuleClass);
+            Objects.requireNonNull(conflict.innerModuleClass);
+            return conflict;
+        }
+
+        public Builder outerModuleClass(ClassName outerModuleClass) {
+            conflict.outerModuleClass = outerModuleClass;
+            return this;
+        }
+
+        public Builder innerModuleClass(ClassName innerModuleClass) {
+            conflict.innerModuleClass = innerModuleClass;
+            return this;
+        }
+    }
 }
